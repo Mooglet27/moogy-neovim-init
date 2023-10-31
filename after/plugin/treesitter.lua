912732f97190faa883a29252046ee3ddc8051582
moogy <christiandefault@gmail.com>
2023-10-31 19:44:51 -0400

    remove help

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,21 +1,20 @@
-require('nvim-treesitter.configs').setup {
-    -- A list of parser names, or "all" (the five listed parsers should always be installed)
-    ensure_installed = { "c", "cpp", "lua", "vim", "help", "query", "python", "rust", "sql", "html", "javascript"},
+require("nvim-treesitter.configs").setup({
+	-- A list of parser names, or "all" (the five listed parsers should always be installed)
+	ensure_installed = { "c", "cpp", "lua", "vim", "query", "python", "rust", "sql", "html", "javascript" },
 
-    -- Install parsers synchronously (only applied to `ensure_installed`)
-    sync_install = false,
+	-- Install parsers synchronously (only applied to `ensure_installed`)
+	sync_install = false,
 
-    -- Automatically install missing parseres when entering buffer
-    auto_install = true,
+	-- Automatically install missing parseres when entering buffer
+	auto_install = true,
 
-    highlight = {
-        enable = true,
+	highlight = {
+		enable = true,
 
-	    additional_vim_regex_highlighting = false,
-    },
-    indent = {
-        enabled = true,
---         disable = { "python" },
-    }
-}
-
+		additional_vim_regex_highlighting = false,
+	},
+	indent = {
+		enabled = true,
+		--         disable = { "python" },
+	},
+})
