75b8529d091561532e987f639d2ece6badeb545b
Christian Irvin <christian.irvin@iridium.com>
2023-11-02 13:33:15 +0000

    update lualine

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,14 +1,14 @@
-require('lualine').setup {
-    -- Place configs here
-    sections = {
-        lualine_a = {'mode'},
-        lualine_b = {'branch', 'diff', 'diagnostics'},
-        lualine_c = {},
-        lualine_x = {'encoding', 'fileformat', 'filetype'},
-        lualine_y = {'progress'},
-        lualine_z = {'location'}
-    },
-    winbar = {
-        lualine_a = {'filename'},
-    }
-}
+require("lualine").setup({
+	options = { theme = "palenight" },
+	sections = {
+		lualine_a = { "mode" },
+		lualine_b = { "branch", "diff", "diagnostics" },
+		lualine_c = {},
+		lualine_x = { "encoding", "fileformat", "filetype" },
+		lualine_y = { "progress" },
+		lualine_z = { "location" },
+	},
+	winbar = {
+		lualine_a = { { "filename", path = 1 } },
+	},
+})
