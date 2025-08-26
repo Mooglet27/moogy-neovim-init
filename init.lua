ce6e5eac6f9284311cb4ceddb7d9d24a85f2b828
Christian Irvin <christian.irvin@iridium.com>
2025-08-26 14:49:34 +0000

    fix: remove diagnotics from init.lua

--- init.lua
+++ init.lua
@@ -1,11 +1,4 @@
 -- require("impatient")
-require("config.basic")
 require("config.lazy")
+require("config.basic")
 require("config.keymap")
-require("config.diagnostics").setup({
-    auto_show_treshold = 3,
-    virtual_text = {
-        prefix = "", -- Change virtual text prefix
-        spacing = 4, -- Adjust spacing
-    },
-})
